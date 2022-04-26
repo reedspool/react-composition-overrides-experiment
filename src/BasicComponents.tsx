aac022f7ae1fae6a3de737b3590414fc28460bb7
Reed Spool <reed@heysparkbox.com>
2022-04-25 22:08:40 -0600

    feature: add components for basic HTML elements

---
+++ src/BasicComponents.tsx
@@ -0,0 +1,8 @@
+export const Button: React.FC<React.PropsWithChildren<any>> =
+    ({ ...rest }) => <button {...rest} />;
+
+export const Anchor: React.FC<React.PropsWithChildren<any>> =
+    ({ ...rest }) => <a {...rest} />;
+
+export const Span: React.FC<React.PropsWithChildren<any>> =
+    ({ ...rest }) => <span {...rest}></span>;
